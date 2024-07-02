145f111a20981b45b565e60c5fb9c8b3e4efd571
Rzjuliannn <lianboy530@gmail.com>
2024-07-02 14:23:44 +0700

    Add files via upload

---
+++ Minggu1/Latihan21.java
@@ -0,0 +1,21 @@
+/** 
+ * Latihan21
+ */
+public class Latihan21 {
+    public static void main(String[] args) {
+        System.out.println("Nama Saya Nabhan Rizqi Julian Saputro");
+        System.out.println("Saya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+
+    // pertanyaan 2
+        System.out.print("Nama Saya Nabhan Rizqi Julian Saputro");
+        System.out.print("Saya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+
+    // pertanyaan 3
+        System.out.print("Nama Saya Nabhan Rizqi Julian Saputro");
+        System.out.print("\nSaya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+ 
+    // pertanyaan 4
+        System.out.print("Nama Saya Nabhan Rizqi Julian Saputro");
+        //System.out.print("\nSaya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+    }
+}
---
+++ Minggu1/identitas21.java
@@ -0,0 +1,11 @@
+public class identitas21 {
+        public static void main(String[] args) {
+            System.out.println("***IDENTITAS DIRI***");
+            System.out.println("Nama       :Nabhan Rizqi Julian Saputro");
+            System.out.println("Absen      :21");
+            System.out.println("Kelas      :1B");
+            System.out.println("Prodi      :D-IV Teknik Informatika");
+            System.out.println("Jurusan    :Teknologi Informasi");
+    
+        }
+    }
